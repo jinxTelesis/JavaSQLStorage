ef6ae3333f939fd6fa781785044f459c42869f6c
jinxtelesis <dremon7@hotmail.com>
2018-08-23 13:03:32 -0400

    gui overhall

--- src/guiS/DbFrame.java
+++ src/guiS/DbFrame.java
@@ -17,6 +17,7 @@ import javax.swing.border.BevelBorder;
 import java.awt.SystemColor;
 import javax.swing.JMenuBar;
 import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 
 public class DbFrame extends JFrame {
 
@@ -28,8 +29,7 @@ public class DbFrame extends JFrame {
 	private JTextField txtStreet;
 	private JTextField txtZip;
 	private JTextField txtStreetAddress_1;
-	private JLabel lblPhone;
-	private JTextField textField_2;
+	private JTextField txtPhone_1;
 	private JTextField txtname;
 	private JTextField txtcompanyName;
 	private JTextField txtStreetAddress;
@@ -41,6 +41,14 @@ public class DbFrame extends JFrame {
 	private JPanel panel_1;
 	private JPanel panel_2;
 	private JTextField textField_3;
+	private JLabel lblQty;
+	private JLabel lblUnit;
+	private JLabel lblTaxed;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_4;
+	private JTextField textField_5;
 
 	/**
 	 * Launch the application.
@@ -74,65 +82,62 @@ public class DbFrame extends JFrame {
 		
 		JLabel lblInvoice = new JLabel("INVOICE");
 		lblInvoice.setFont(new Font("Tahoma", Font.BOLD, 24));
-		lblInvoice.setBounds(616, 11, 134, 29);
+		lblInvoice.setBounds(566, 29, 134, 29);
 		contentPane.add(lblInvoice);
 		
 		txtnumber = new JTextField();
 		txtnumber.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		txtnumber.setText("[number]");
-		txtnumber.setBounds(579, 78, 86, 20);
+		txtnumber.setBounds(529, 96, 86, 20);
 		contentPane.add(txtnumber);
 		txtnumber.setColumns(10);
 		
 		txtDate = new JTextField();
 		txtDate.setText("1/1/2018");
-		txtDate.setBounds(675, 78, 86, 20);
+		txtDate.setBounds(625, 96, 98, 20);
 		contentPane.add(txtDate);
 		txtDate.setColumns(10);
 		
 		txtCompanyName = new JTextField();
 		txtCompanyName.setText("Company Name");
 		txtCompanyName.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		txtCompanyName.setBounds(64, 50, 146, 29);
+		txtCompanyName.setBounds(44, 51, 146, 29);
 		contentPane.add(txtCompanyName);
 		txtCompanyName.setColumns(10);
 		
 		txtCity = new JTextField();
 		txtCity.setText("City");
-		txtCity.setBounds(64, 130, 86, 20);
+		txtCity.setBounds(44, 131, 86, 20);
 		contentPane.add(txtCity);
 		txtCity.setColumns(10);
 		
 		txtStreet = new JTextField();
 		txtStreet.setText("Street");
-		txtStreet.setBounds(160, 130, 86, 20);
+		txtStreet.setBounds(140, 131, 86, 20);
 		contentPane.add(txtStreet);
 		txtStreet.setColumns(10);
 		
 		txtZip = new JTextField();
 		txtZip.setText("Zip");
-		txtZip.setBounds(256, 130, 86, 20);
+		txtZip.setBounds(236, 131, 86, 20);
 		contentPane.add(txtZip);
 		txtZip.setColumns(10);
 		
 		txtStreetAddress_1 = new JTextField();
 		txtStreetAddress_1.setText("Street Address");
-		txtStreetAddress_1.setBounds(64, 95, 86, 20);
+		txtStreetAddress_1.setBounds(44, 96, 86, 20);
 		contentPane.add(txtStreetAddress_1);
 		txtStreetAddress_1.setColumns(10);
 		
-		lblPhone = new JLabel("Phone");
-		lblPhone.setBounds(64, 161, 52, 20);
-		contentPane.add(lblPhone);
-		
-		textField_2 = new JTextField();
-		textField_2.setBounds(126, 161, 124, 20);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
+		txtPhone_1 = new JTextField();
+		txtPhone_1.setText("Phone");
+		txtPhone_1.setBounds(44, 162, 124, 20);
+		contentPane.add(txtPhone_1);
+		txtPhone_1.setColumns(10);
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel.setBounds(64, 217, 458, 29);
+		panel.setBounds(44, 217, 458, 29);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -143,62 +148,64 @@ public class DbFrame extends JFrame {
 		
 		txtname = new JTextField();
 		txtname.setText("[Name]");
-		txtname.setBounds(64, 257, 86, 20);
+		txtname.setBounds(44, 257, 86, 20);
 		contentPane.add(txtname);
 		txtname.setColumns(10);
 		
 		txtcompanyName = new JTextField();
 		txtcompanyName.setText("[Company Name]");
-		txtcompanyName.setBounds(64, 288, 157, 20);
+		txtcompanyName.setBounds(44, 288, 157, 20);
 		contentPane.add(txtcompanyName);
 		txtcompanyName.setColumns(10);
 		
 		txtStreetAddress = new JTextField();
 		txtStreetAddress.setText("Street Address");
-		txtStreetAddress.setBounds(64, 319, 86, 20);
+		txtStreetAddress.setBounds(44, 319, 86, 20);
 		contentPane.add(txtStreetAddress);
 		txtStreetAddress.setColumns(10);
 		
 		txtcity = new JTextField();
 		txtcity.setText("[City]");
-		txtcity.setBounds(64, 354, 86, 20);
+		txtcity.setBounds(44, 353, 86, 20);
 		contentPane.add(txtcity);
 		txtcity.setColumns(10);
 		
 		txtStreet_1 = new JTextField();
 		txtStreet_1.setText("Street");
-		txtStreet_1.setBounds(164, 354, 86, 20);
+		txtStreet_1.setBounds(144, 353, 86, 20);
 		contentPane.add(txtStreet_1);
 		txtStreet_1.setColumns(10);
 		
 		txtZip_1 = new JTextField();
 		txtZip_1.setText("Zip");
-		txtZip_1.setBounds(267, 354, 86, 20);
+		txtZip_1.setBounds(247, 353, 86, 20);
 		contentPane.add(txtZip_1);
 		txtZip_1.setColumns(10);
 		
 		txtPhone = new JTextField();
 		txtPhone.setText("[Phone]");
-		txtPhone.setBounds(64, 385, 86, 20);
+		txtPhone.setBounds(44, 384, 86, 20);
 		contentPane.add(txtPhone);
 		txtPhone.setColumns(10);
 		
 		txtemailAddress = new JTextField();
 		txtemailAddress.setText("[Email Address]");
-		txtemailAddress.setBounds(64, 416, 86, 20);
+		txtemailAddress.setBounds(44, 415, 86, 20);
 		contentPane.add(txtemailAddress);
 		txtemailAddress.setColumns(10);
 		
 		panel_1 = new JPanel();
-		panel_1.setBounds(579, 51, 92, 20);
+		panel_1.setBounds(529, 69, 86, 20);
 		contentPane.add(panel_1);
 		
 		JLabel lblInvoiceId = new JLabel("Invoice ID");
+		lblInvoiceId.setHorizontalAlignment(SwingConstants.LEFT);
 		panel_1.add(lblInvoiceId);
 		lblInvoiceId.setBackground(SystemColor.menu);
+		lblInvoiceId.setBounds(530,69,86,20);
 		
 		panel_2 = new JPanel();
-		panel_2.setBounds(681, 51, 92, 20);
+		panel_2.setBounds(625, 69, 98, 20);
 		contentPane.add(panel_2);
 		
 		JLabel lblNewLabel = new JLabel("Date");
@@ -210,23 +217,29 @@ public class DbFrame extends JFrame {
 		contentPane.add(panel_3);
 		panel_3.setLayout(null);
 		
-		JLabel lblNewLabel_2 = new JLabel("Description");
+		JLabel lblNewLabel_2 = new JLabel("Item Description");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblNewLabel_2.setBounds(10, 11, 86, 14);
+		lblNewLabel_2.setBounds(10, 10, 113, 15);
 		panel_3.add(lblNewLabel_2);
 		
-		JLabel lblAmout = new JLabel("amount");
+		JLabel lblAmout = new JLabel(" Amount");
 		lblAmout.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblAmout.setBounds(613, 12, 72, 14);
+		lblAmout.setBounds(596, 9, 89, 14);
 		panel_3.add(lblAmout);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(44, 480, 543, 266);
-		contentPane.add(textArea);
+		lblQty = new JLabel(" Qty");
+		lblQty.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblQty.setBounds(352, 10, 72, 15);
+		panel_3.add(lblQty);
 		
-		JTextArea textArea_1 = new JTextArea();
-		textArea_1.setBounds(597, 480, 142, 266);
-		contentPane.add(textArea_1);
+		lblUnit = new JLabel(" Unit $");
+		lblUnit.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblUnit.setBounds(434, 10, 61, 15);
+		panel_3.add(lblUnit);
+		
+		lblTaxed = new JLabel(" Taxed");
+		lblTaxed.setBounds(515, 11, 46, 14);
+		panel_3.add(lblTaxed);
 		
 		JLabel lblThankYouFor = new JLabel("Thank you for your purchase!");
 		lblThankYouFor.setForeground(Color.BLACK);
@@ -244,6 +257,31 @@ public class DbFrame extends JFrame {
 		textField_3.setBounds(653, 804, 86, 20);
 		contentPane.add(textField_3);
 		textField_3.setColumns(10);
+		
+		textField = new JTextField();
+		textField.setBounds(44, 486, 345, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(399, 486, 70, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(479, 486, 70, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(559, 486, 70, 20);
+		contentPane.add(textField_4);
+		textField_4.setColumns(10);
+		
+		textField_5 = new JTextField();
+		textField_5.setBounds(639, 486, 100, 20);
+		contentPane.add(textField_5);
+		textField_5.setColumns(10);
 		setTitle("Joan's SQL PRO");
 	}
 }
